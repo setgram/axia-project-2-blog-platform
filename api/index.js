fb69b94a69bfebea22d44697c666fc6297814a8b
Wisdom Okogho <wisdommokoghoo@gmail.com>
2025-03-13 15:38:45 +0100

    database connected

--- api/index.js
+++ api/index.js
@@ -1,4 +1,17 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("connected to db");
+  })
+  .catch(() => {
+    console.log("something is wrong");
+  });
 
 const app = express();
 
